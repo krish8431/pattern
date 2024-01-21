680e3f76a22bf806f686b140d75c74b3511041a3
krish8431 <krish8431@gmail.com>
2024-01-21 16:37:11 +0530

    Modified

--- Demo.java
+++ Demo.java
@@ -1,15 +1,15 @@
 public class Demo{
     public static void main(String args[]){
-        int no_Of_Students=200;
-        int $lol_=no_Of_Students + 500;
-        int a=84;
-        double b=a;
-        double num1=84.0;
-        int num2=(int)num1;
-        double result=$lol_ / num2;
-        boolean ufff=$lol_>no_Of_Students;
-        int c=10;
-        int d=3;
+        // int no_Of_Students=200;
+        // int $lol_=no_Of_Students + 500;
+        // int a=84;
+        // double b=a;
+        // double num1=84.0;
+        // int num2=(int)num1;
+        // double result=$lol_ / num2;
+        // boolean ufff=$lol_>no_Of_Students;
+        // int c=10;
+        // int d=3;
        //System.out.println(c++);
        //System.out.println(++c);
     //System.out.println(c % d);
@@ -23,28 +23,28 @@ public class Demo{
         //System.out.println($lol_);
        // System.out.println(num1);
         //System.out.print(num2);
-        int e=12;
-        int f=e+=20;
+        // int e=12;
+        // int f=e+=20;
         //System.out.println(f);
         //System.out.println(f-=9);
-        int boo=10;
-        int boo1=10;
-        int sanku=12;
-        double gawar=12/7;
-        int check=(int)gawar*10;
-        System.out.println(check);
-        System.out.println(gawar);
-        System.out.println(boo1);
-        System.out.println(boo);
-        System.out.println(boo==boo1); 
-        System.out.println(10<=boo1);
-        System.out.println(boo1>=1);
-        System.out.println(boo1!=boo);
-        System.out.println(boo==boo1 && boo!=boo1);     
-        System.out.println(boo1<boo || boo>=boo1);
-        System.out.println(10==10 != 10>2 != 10<=3);
-        System.out.println(!false);
+    //     int boo=10;
+    //     int boo1=10;
+    //     int sanku=12;
+    //     double gawar=12/7;
+    //     int check=(int)gawar*10;
+    //     System.out.println(check);
+    //     System.out.println(gawar);
+    //     System.out.println(boo1);
+    //     System.out.println(boo);
+    //     System.out.println(boo==boo1); 
+    //     System.out.println(10<=boo1);
+    //     System.out.println(boo1>=1);
+    //     System.out.println(boo1!=boo);
+    //     System.out.println(boo==boo1 && boo!=boo1);     
+    //     System.out.println(boo1<boo || boo>=boo1);
+    //     System.out.println(10==10 != 10>2 != 10<=3);
+    //     System.out.println(!false);
 
-    }
+    // }
 
 }
